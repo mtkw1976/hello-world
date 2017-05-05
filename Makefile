7bfc7be89d71cff0ed9677c01e8977ece33d169e
mtkw1976 <mtkw1976@gmail.com>
2017-05-05 16:21:46 +0900

    change main.c to hello.c

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC := gcc
+
+hello: hello.c
+	gcc -o hello hello.c
--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello CircleCI!!!\r\n");
+  printf("Hello CircleCI!!!!\r\n");
+  return 0;
 }
